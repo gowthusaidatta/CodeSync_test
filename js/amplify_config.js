e98d53a8cf0e631015306539a218c68bb3a63fa8
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2025-07-25 22:57:58 +0530

    Update amplify_config.js

--- js/amplify_config.js
+++ js/amplify_config.js
@@ -1,9 +1,8 @@
-// Define config as a global variable on window
 window.amplifyConfig = {
   Auth: {
     region: "us-east-1",
     userPoolId: "us-east-1_RMhlKuGHS",
     userPoolWebClientId: "4g0bo9ac68vq4g817v1je4e5sf",
-    authenticationFlowType: "USER_PASSWORD_AUTH",
+    authenticationFlowType: "USER_PASSWORD_AUTH"
   }
 };
