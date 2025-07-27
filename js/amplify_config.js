9bdec5d4e8905f580f4f25756d8008480a24d975
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2025-07-27 15:06:15 +0530

    Delete js/amplify_config.js

--- js/amplify_config.js
+++
@@ -1,9 +0,0 @@
-// js/amplify_config.js
-window.amplifyConfig = {
-  Auth: {
-    region: "us-east-1",
-    userPoolId: "us-east-1_RMhlKuGHS",
-    userPoolWebClientId: "4g0bo9ac68vq4g817v1je4e5sf",
-    authenticationFlowType: "USER_PASSWORD_AUTH"
-  }
-};
