60598d7425032ff1dfe4cb60065fbee4d41d682f
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2025-07-30 23:03:35 +0530

    Update amplify_config.js

--- js/amplify_config.js
+++ js/amplify_config.js
@@ -1,9 +1,20 @@
-// js/amplify_config.js
+// // js/amplify_config.js
+// window.amplifyConfig = {
+//   Auth: {
+//     region: "us-east-1",
+//     userPoolId: "us-east-1_RMhlKuGHS",
+//     userPoolWebClientId: "4g0bo9ac68vq4g817v1je4e5sf",
+//     authenticationFlowType: "USER_PASSWORD_AUTH"
+//   }
+// };
+
+
 window.amplifyConfig = {
   Auth: {
     region: "us-east-1",
     userPoolId: "us-east-1_RMhlKuGHS",
     userPoolWebClientId: "4g0bo9ac68vq4g817v1je4e5sf",
-    authenticationFlowType: "USER_PASSWORD_AUTH"
+    authenticationFlowType: "USER_PASSWORD_AUTH",
+    storage: window.localStorage  // ✅ this is the fix!
   }
 };
